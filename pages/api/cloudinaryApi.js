6e55a83975bb68c8dc2fbd6edea2c60c59fa0e8d
Divine <dpkreativ@gmail.com>
2022-04-20 02:15:14 +0100

    write API logic to handle text detection

--- pages/api/cloudinaryApi.js
+++ pages/api/cloudinaryApi.js
@@ -5,3 +5,24 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
 });
+
+export default (request, response) => {
+  const image = request.body;
+
+  return cloudinary.v2.uploader.upload(
+    image,
+    { ocr: 'adv_ocr' },
+    (error, result) => {
+      if (error) return response.status(500).json({ error });
+
+      const { textAnnotations } = result.info.ocr.adv_ocr.data[0];
+
+      const extractedText = textAnnotations
+        .map((anno, i) => i > 0 && anno.description.replace(/[^0-9a-z]/gi, ''))
+        .filter((entry) => typeof entry === 'string')
+        .join(' ');
+
+      return response.status(200).json({ extractedText });
+    }
+  );
+};
